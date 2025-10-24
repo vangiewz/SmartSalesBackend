0b391c897c537b40671078fe26202daf4dc0f6b1
Leonardo Serrate <leonardoserrate9@gmail.com>
2025-10-24 12:21:48 -0400

    plantilla a;adida

--- smartsales/ai_reports/models.py
+++ smartsales/ai_reports/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class PlantillaReporte(models.Model):
+    usuario = models.ForeignKey(
+        'smartsales.Usuario', # <-- nombre correcto de la app/modelo
+        on_delete=models.CASCADE,
+        db_column='usuario_id'
+    )
+    nombre = models.CharField(max_length=120)
+    prompt = models.TextField()
+    formato = models.CharField(max_length=12, blank=True, null=True)
+    filtros = models.JSONField(blank=True, null=True)
+    creado_en = models.DateTimeField()
+    actualizado_en = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = "plantilla_reporte"
--- smartsales/ai_reports/urls.py
+++ smartsales/ai_reports/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
-from .views import RunReportView, RunAudioReportView
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import RunReportView, RunAudioReportView, PlantillaReporteViewSet
+
+router = DefaultRouter()
+router.register(r'plantillas', PlantillaReporteViewSet, basename='plantillas')
 
 urlpatterns = [
     path('run', RunReportView.as_view(), name='ai_reports_run'),
     path('run-audio', RunAudioReportView.as_view(), name='ai_reports_run_audio'),
+    path('', include(router.urls)),   # <-- aquí agregas el router
 ]
